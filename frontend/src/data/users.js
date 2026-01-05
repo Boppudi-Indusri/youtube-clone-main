2fe22459545ab93c3ef6f38d9be8454b6b5f9509
Boppudi-Indusri <92775014+Boppudi-Indusri@users.noreply.github.com>
2026-01-05 15:00:48 +0530

    Create users.js

---
+++ frontend/src/data/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    userId: "user01",
+    username: "JohnDoe",
+    email: "john@example.com",
+    password: "hashedPassword123",
+    avatar: "https://i.pravatar.cc/150?img=3",
+    channels: ["channel01"],
+  },
+];
+
+export default users;
